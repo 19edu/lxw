b4a37ae8367c76652142b0e04cc6cfb9f6bedab5
liangquanqing <liangquanqing@coocaa.com>
2019-06-21 11:24:25 +0800

    加入一些文件

---
+++ js/play.js
@@ -0,0 +1,30 @@
+var _curLevel = 5;				// 当前解锁了第几关，0为都没有解锁，1为第一关已经解锁，2为第二关已经解锁 ...    
+
+
+function updateMainPage()					// 刷新主界面 
+{
+	var i;
+	// 底部的条栏
+	$("#bottomBar").css("display","block");
+	if (_curLevel < 10) {
+		for (i = _curLevel; i < 10; i++) {
+			$("#objice" + i).css("display", "block");
+			$("#objlock" + i).css("display", "block");
+		}
+	}
+	//$("#objlck4").removeClass("noshow");
+	//$("#objlck4").addClass("show");
+	
+	/*
+	map = new coocaakeymap($(".coocaa_btn"), 
+		$(".level:eq(" + rememberMapFocus + ")"), 
+		"btnFocus", 
+		function() {}, 
+		function(val) {}, 
+		function(obj) {}
+	);
+	*/
+   map = new coocaakeymap($(".coocaa_btn"), "#mainpageButton2", "btn-focus", function() {}, function(val) {}, function(obj) {});
+   
+}
+
